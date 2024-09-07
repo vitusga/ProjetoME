001e6994a20313d39249d3bfe495589f3f76fbf5
Vítor <68357527+vitusga@users.noreply.github.com>
2024-09-07 01:42:22 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+function armazenarValor() {
+  const inputTexto = document.getElementById('email');
+  const valorDigitado = inputTexto.value;
+  const valorArmazenado = valorDigitado;
+  console.log(valorArmazenado,'Salvo com sucesso!');
+}
+
+if (!email(0)) {
+
+    return
+}
